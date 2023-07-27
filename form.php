20c1c842abd692d87a1fd72da388a99df9c5922f
ThierrydelCastillo <dev@del-castillo.fr>
2023-07-27 08:41:37 +0200

    04 - Bien documenter ses classes

--- form.php
+++ form.php
@@ -1,25 +1,62 @@
 <?php
-
+/**
+ * class Form
+ * Permet de générer un formulaire rapidement
+ */
 class Form {
 
+    /**
+     * Données utilisée par le formulaire
+     * 
+     * @var array 
+     */
     private $data;
+
+    /**
+     * Tag utilisé pour entourer les champs
+     *
+     * @var string
+     */
     public $surround = 'p';
 
+    /**
+     * Données utilisée par le formulaire
+     *
+     * @param array $data
+     */
     public function __construct($data = array( ))
     {
         $this->data = $data;    
     }
 
+    /**
+     * Entoure le code html du tag $this->surround
+     *
+     * @param string $html 
+     * @return string
+     */
     private function surround($html)
     {
         return "<$this->surround>$html</$this->surround>";
     }
 
+    /**
+     * Retourne la valeur dun index de $this->data
+     *
+     * @param string $index
+     * @return string
+     */
     private function getValue($index)
     {
         return isset($this->data[$index]) ? $this->data[$index] : null;
     }
 
+    /**
+     * retourne un <input>
+     *
+     * @param string $name
+     * @return string
+     */
     public function input($name)
     {
         return $this->surround(
@@ -28,6 +65,11 @@ class Form {
 
     }
 
+    /**
+     * retourn un <button pour soumettre le formulaire 
+     *
+     * @return string
+     */
     public function submit()
     {
         return $this->surround('<button type="submit">Envoyer</button>');
